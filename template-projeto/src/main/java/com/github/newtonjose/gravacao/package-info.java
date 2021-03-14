47631132fd3abb8ba7799706c152550a18e452b2
newtonjose <josephnewton001@gmail.com>
2021-03-14 02:21:09 -0300

    feat: template para projetos maven

---
+++ template-projeto/src/main/java/com/github/newtonjose/gravacao/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * Pacote do repositório de "gravacao". Contém duas classes
+ * apenas: (a) {@link com.github.newtonjose.gravacao.domain.Calendario} e
+ * (b) {@link com.github.newtonjose.gravacao.application.console.ProgramaCalendario}.
+ * Observe que essa última classe apenas ilustra como um
+ * programa em Java pode chamar o código da classe
+ * relevante ({@link com.github.newtonjose.gravacao.domain.Calendario}).
+ *
+ * @see com.github.newtonjose.gravacao.domain.Calendario
+ * @see com.github.newtonjose.gravacao.application.console.ProgramaCalendario
+ */
+package com.github.newtonjose.gravacao;
