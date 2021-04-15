f1f005ed84003e9b83f798baf477153fbad96705
newtonjose <josephnewton001@gmail.com>
2021-04-15 20:18:19 -0300

    feat: candidato model

---
+++ java-web-tomcat/src/main/java/github/newtonjose/domain/models/Candidato.java
@@ -0,0 +1,39 @@
+package main.java.github.newtonjose.domain.models;
+
+import java.time.LocalDate;
+
+public class Candidato {
+    private String name;
+    private char gender;
+    private LocalDate bornDate;
+    private String jobPosition;
+    private String curriculumnResume;
+
+    public Candidato(String name, char gender, LocalDate date, String cargo, String resume) {
+        this.name = name;
+        this.gender = gender;
+        this.bornDate = date;
+        this.jobPosition = cargo;
+        this.curriculumnResume = resume;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public char getGender() {
+        return this.gender;
+    }
+
+    public LocalDate getBornDate() {
+        return this.bornDate;
+    }
+
+    public String getJobPosition() {
+        return this.jobPosition;
+    }
+
+    public String getCurriculumResume() {
+        return this.curriculumnResume;
+    }
+}
