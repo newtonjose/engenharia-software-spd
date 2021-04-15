e10def67aea34dd5b4e85323cc49d2f92f5c7bd7
newtonjose <josephnewton001@gmail.com>
2021-04-15 02:41:52 -0300

    feat: getting parameters from request

--- java-web-tomcat/src/main/java/servlets/CadastrarCandidato.java
+++ java-web-tomcat/src/main/java/servlets/CadastrarCandidato.java
@@ -6,10 +6,23 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 @WebServlet(name = "CadastrarCandidato", urlPatterns = {"/candidatos/cadastro"})
 public class CadastrarCandidato extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.sendRedirect("/index.html");
+
+        String name = request.getParameter("inputName");
+        String gender = request.getParameter("radioOptions");
+        LocalDate date = LocalDate.parse(
+                request.getParameter("datePicker"),
+                DateTimeFormatter.ofPattern("dd/MM/yyyy")
+        );
+        String cargo = request.getParameter("inputCargo");
+        String resume = request.getParameter("inputResumo");
+
+        System.out.println(date);
+//        response.sendRedirect("/index.html");
     }
 }
